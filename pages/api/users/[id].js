a1416ccbb515f6a6301feb00fa6e8b20bd541177
bramahendramahendra <bramahendramahendra@gmail.com>
2023-12-13 22:07:15 +0700

    delete data API

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -1,3 +1,4 @@
+import { deleteUser } from "@/services/user/deleteUser";
 import { getUser } from "@/services/user/getUser";
 import { updateUser } from "@/services/user/updateUser";
 
@@ -10,6 +11,9 @@ export default async function handler(req, res) {
     } else if (req.method === 'PUT') {
         const updatedUser = await updateUser(id, req.body);
         res.status(200).json(updatedUser);
+    } else if (req.method === 'DELETE') {
+        await deleteUser(id);
+        res.status(200).send('User deleted successfully');
     } else {
         res.setHeader('Allow', ['GET', 'PUT', 'DELETE']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
--- services/user/deleteUser.js
+++ services/user/deleteUser.js
@@ -1,14 +1,12 @@
 import { connectToDatabase } from '@/lib/db';
 
-export async function updateUser(userId, updateData) {
+export async function deleteUser(userId) {
     const db = await connectToDatabase();
-    
-    const [result] = await db.query('UPDATE users SET ? WHERE id = ?', [updateData, userId]);
+
+    const [result] = await db.query('DELETE FROM users WHERE id = ?', [userId]);
     await db.end();
 
     if (result.affectedRows === 0) {
         throw new Error('User not found');
     }
-
-    return result;
 }
